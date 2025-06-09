555e3ca6a501253c45d8fbabae4f2a4965aa4c51
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2025-06-09 09:23:15 -0600

    added a if-else to handle if we can not find a word

--- app1.py
+++ app1.py
@@ -3,7 +3,10 @@ import json
 data = json.load(open("data.json"))
 
 def translate(word):
-    return data[word]
+    if word in data:
+        return data[word]
+    else:
+        return "The word you entered does not exist. Please double check it."
 
 word = input("Enter word: ")
 
