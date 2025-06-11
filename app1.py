8a389beec845ac0212d49807e9dd50b00bf68c1a
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2025-06-11 14:31:23 -0600

    added more logic for user confimation and refactored our if-elif-else statement

--- app1.py
+++ app1.py
@@ -8,7 +8,9 @@ def translate(word):
     if word in data:
         return data[word]
     elif len(get_close_matches(word, data.keys())) > 0:
-        return input("Did you mean %s instead? Enter Y if yes, or N if no." %get_close_matches(word, data.keys())[0])
+        yn =  input("Did you mean %s instead? Enter Y if yes, or N if no: " % get_close_matches(word, data.keys())[0])
+        if yn == 'Y':
+            return data[get_close_matches(word, data.keys())[0]]
     else:
         return "The word you entered does not exist. Please double check it."
 
