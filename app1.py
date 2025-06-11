9489727e7c7a48503f57d3e7dfabbd61c137eca8
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2025-06-11 12:16:31 -0600

    adding logic for close matches. While we searching the data.json

--- app1.py
+++ app1.py
@@ -1,4 +1,5 @@
 import json
+import difflib import get_close_matches
 
 data = json.load(open("data.json"))
 
@@ -6,6 +7,8 @@ def translate(word):
     word = word.lower()
     if word in data:
         return data[word]
+    elif len(get_close_matches(word, data.keys())) > 0:
+        return "Did you mean %s instead?" %get_close_matches(word, data.keys())[0]
     else:
         return "The word you entered does not exist. Please double check it."
 
